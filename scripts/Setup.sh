983402db5ba9b319c2bbd0860f50a88b987c95e3
JentHS <538773@student.saxion.nl>
2025-03-29 14:30:17 +0100

    move Setup.sh and update

---
+++ scripts/Setup.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+DB_USERNAME="TestTest"
+DB_PASSWORD="TestTest"
+REGION="us-east-1"
+
+# Deploy Stack 1: AZStack
+echo "Creating stack: AZStack"
+aws cloudformation create-stack --stack-name "AZStack" --template-body file://az.yaml --region "$REGION"
+aws cloudformation wait stack-create-complete --stack-name "AZStack" --region "$REGION"
+echo "AZStack deployment is complete!"
+
+# Deploy Stack 2: DatabaseStack
+echo "Creating stack: DatabaseStack"
+aws cloudformation create-stack --stack-name "DatabaseStack" \
+  --template-body file://rds.yml \
+  --parameters ParameterKey=DBUsername,ParameterValue="$DB_USERNAME" \
+               ParameterKey=DBPassword,ParameterValue="$DB_PASSWORD" \
+  --region "$REGION"
+aws cloudformation wait stack-create-complete --stack-name "DatabaseStack" --region "$REGION"
+echo "DatabaseStack deployment is complete!"
+
+# Deploy Stack 3: LBStack
+echo "Creating stack: LBStack"
+aws cloudformation create-stack --stack-name "LBStack" --template-body file://lb.yml --region "$REGION"
+aws cloudformation wait stack-create-complete --stack-name "LBStack" --region "$REGION"
+echo "LBStack deployment is complete!"
+
+# Deploy Stack 4: EFSStack
+echo "Creating stack: EFSStack"
+aws cloudformation create-stack --stack-name "EFSStack" --template-body file://efs.yml --region "$REGION"
+aws cloudformation wait stack-create-complete --stack-name "EFSStack" --region "$REGION"
+echo "EFSStack deployment is complete!"
+
+# Deploy Stack 5: ASGStack
+echo "Creating stack: ASGStack"
+aws cloudformation create-stack --stack-name "ASGStack" --template-body file://asg.yaml --region "$REGION"
+aws cloudformation wait stack-create-complete --stack-name "ASGStack" --region "$REGION"
+echo "ASGStack deployment is complete!"
+
+# Deploy Stack 6: ELKStack
+echo "Creating stack: ELKStack"
+aws cloudformation create-stack --stack-name "ELKStack" --template-body file://elk.yaml --region "$REGION"
+aws cloudformation wait stack-create-complete --stack-name "ELKStack" --region "$REGION"
+echo "ELKStack deployment is complete!"
+
+echo "All stacks have been deployed!"
