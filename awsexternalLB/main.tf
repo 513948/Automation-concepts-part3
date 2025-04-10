2054ed0882c78c5da73a092ebee2d25318908735
Geordi Schoenmakers <120180681+513948@users.noreply.github.com>
2025-04-10 22:40:12 +0200

    Update and rename LB.tf to main.tf

---
+++ awsexternalLB/main.tf
@@ -0,0 +1,8 @@
+data "local_file" "glb_template" {
+  filename = "${path.module}/glb.yaml"
+}
+
+resource "aws_cloudformation_stack" "GLBStack" {
+  name          = "GLBStack"
+  template_body = data.local_file.glb_template.content
+}
